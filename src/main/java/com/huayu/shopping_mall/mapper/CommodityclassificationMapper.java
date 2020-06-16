3433122d26bcd745bfef621847bc59b335a13ffb
mengQi <1357885037@qq.com>
2020-06-16 10:12:42 +0800

    mq 6/16 早

--- src/main/java/com/huayu/shopping_mall/mapper/CommodityclassificationMapper.java
+++ src/main/java/com/huayu/shopping_mall/mapper/CommodityclassificationMapper.java
@@ -3,6 +3,9 @@ package com.huayu.shopping_mall.mapper;
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import com.huayu.shopping_mall.entity.Commodityclassification;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Select;
+
+import java.util.List;
 
 /**
  * <p>
@@ -14,5 +17,12 @@ import org.apache.ibatis.annotations.Mapper;
  */
 @Mapper
 public interface CommodityclassificationMapper extends BaseMapper<Commodityclassification> {
+
+    //查询卖出去的商品分类
+    @Select(" select ccname from  commodityclassification where ccid in(select  ccid from commodity where cid in (select cid from orderform where ofstate!=-1 group by cid) group by ccid)  ")
+    List<String> commodityClassification();
+
+
+
 
 }
